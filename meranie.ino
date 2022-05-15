ba531e6bf6027edee41a53caf4e94725df69dcc7
mgrofik <105544166+mgrofik@users.noreply.github.com>
2022-05-15 15:27:51 +0200

    Add files via upload

---
+++ meranie.ino
@@ -0,0 +1,45 @@
+// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
+// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#include "DHT.h"
+
+#define seaLevelPressure_hPa 1013.25
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22   // DHT 22  
+
+// Initialize DHT and bmp sensor.
+
+DHT dht(DHTPIN, DHTTYPE);
+Adafruit_BMP085 bmp;
+
+void setup() {
+  Serial.begin(9600);
+  //Serial.println(F("DHTxx test!"));
+  dht.begin();
+  bmp.begin();
+}
+
+void loop() {
+  // Wait a 2 seconds between measurements.
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  //Serial.print(F("Humidity: "));
+  Serial.println(h);
+ // Serial.print(" ");
+ // Serial.print(F("%  Temperature: "));
+  Serial.println(t);
+  //Serial.print("Pressure = ");
+  Serial.println(bmp.readPressure());
+   // Serial.println(" Pa");
+  
+}
