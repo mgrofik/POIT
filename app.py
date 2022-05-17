c2e9edebf17566b13d491fd2f1504abb23ecc8f4
mgrofik <105544166+mgrofik@users.noreply.github.com>
2022-05-17 19:51:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ from flask import Flask, render_template, session, request, jsonify, url_for
 from flask_socketio import SocketIO, emit, disconnect
 import MySQLdb       
 import math
-import time
 import configparser as ConfigParser
-import random
 import serial
 
 async_mode = None
@@ -36,17 +34,15 @@ def background_thread(args):
     q = 0
     dataList = []  
     db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
-    dbV = 'nieco' 
+    dbV = 'nieco'
+    Op = 'nieco1'
     while True:
         if args:
             print('Moje args: ')
             print(args)
-            #A = dict(args).get('A')
-            dbV = dict(args).get('db_value')         
-        #else:
-        #    print('nemam args')
-        A = 1
-        #print A
+            Op = dict(args).get('Op')
+            dbV = dict(args).get('db_value')              
+        print(Op)
         print(dbV) 
         print(args)
         socketio.sleep(2)
@@ -84,12 +80,9 @@ def background_thread(args):
             fo.close()
           dataList = []
           dataCounter = 0
-        socketio.emit('my_response', {'datah': h, 'datat': t,'datap': p,'datax': dataCounter,'dataq': q}, namespace='/test')  
+        if Op == 'open':
+            socketio.emit('my_response', {'datah': h, 'datat': t,'datap': p,'datax': dataCounter,'dataq': q}, namespace='/test')  
     db.close()
-
-#@app.route('/')
-#def index():
- #   return render_template('index.html', async_mode=socketio.async_mode)
 
 @app.route('/')
 def hello():
@@ -98,27 +91,6 @@ def hello():
 @app.route('/index', methods=['GET', 'POST'])
 def gauge():
     return render_template('index.html', async_mode=socketio.async_mode)
-
-# @app.route('/write')
-# def write2file():
-  #   fo = open("static/files/test.txt","a+")
-    #for i in dataList:
-   #    fo.write(dataList[i])
-    # fuj = '%fuj'
-    # fo.write("%s\r\n" %fuj)
-#     return "done"
-
-# @app.route('/graph', methods=['GET', 'POST'])
-# def graph():
-#     return render_template('graph.html', async_mode=socketio.async_mode)
-
-# @app.route('/graphlive', methods=['GET', 'POST'])
-# def graphlive():
-#    return render_template('graphlive.html', async_mode=socketio.async_mode)
-
-#@app.route('/gauge', methods=['GET', 'POST'])
-#def gauge():
-#    return render_template('gauge.html', async_mode=socketio.async_mode)
 
 @app.route('/db')
 def db():
@@ -137,21 +109,23 @@ def dbdata(num):
   rv = cursor.fetchone()
   return str(rv[0])
     
-@socketio.on('my_event', namespace='/test')
-def test_message(message):   
-    session['receive_count'] = session.get('receive_count', 0) + 1 
-    session['A'] = message['value']    
-    emit('my_response',
-         {'data': message['value'], 'count': session['receive_count']})
-
 @socketio.on('db_event', namespace='/test')
+def db_message(message):
+    print('teraz message: ')
+    print(message)
+    #dbV = message['value']
+    #session['receive_count'] = session.get('receive_count', 0) + 1 
+    session['db_value'] = message['value']    
+    #emit('my_response',
+      #   {'data': message['value'], 'count': session['receive_count']})
+@socketio.on('Op_event', namespace='/test')
 def db_message(message):
     #print('moj message: ', message)
     print('teraz message: ')
     print(message)
     #dbV = message['value']
     #session['receive_count'] = session.get('receive_count', 0) + 1 
-    session['db_value'] = message['value']    
+    session['Op'] = message['value']    
     #emit('my_response',
       #   {'data': message['value'], 'count': session['receive_count']})
 @app.route('/read/<string:num>')
@@ -176,11 +150,9 @@ def test_connect():
             thread = socketio.start_background_task(target=background_thread, args=session._get_current_object())
    # emit('my_response', {'data': 'Connected', 'count': 0})
 
-
 @socketio.on('disconnect', namespace='/test')
 def test_disconnect():
     print('Client disconnected', request.sid)
-
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=80, debug=True)
